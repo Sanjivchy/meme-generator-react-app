342d03e5fd286fba6db7e2617f72452f33be0c02
xoonziip <sanjivchaudhary416@gmail.com>
2021-03-26 21:32:16 +0545

    api fecthed

--- src/components/MemeGenetator.js
+++ src/components/MemeGenetator.js
@@ -7,16 +7,47 @@ class MemeGenetator extends Component {
         this.state = {
          topText:"",
          bottomText:"",
-         randomImg:"http://i.imgflip.com/1bij.jpg"
+         randomImg:"http://i.imgflip.com/1bij.jpg",
+        allMemeImgs: []
         }
+            this.hangleChange= this.hangleChange.bind(this)
     }
-    
+
+    componentDidMount(){
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response =>{
+                const {memes}= response.data
+                this.setState({
+                    allMemeImgs: memes
+                })
+            })
+    }
+hangleChange(event){
+console.log("working")
+}
     
     render() {
         return (
             <div>
-                <Header/>
-                <h1>meme generator</h1>
+               <Header/>
+
+                <form className="meme-form">
+                    <input type="text"
+                    name="topText"
+                    placeholder="Top text"
+                    value={this.state.topText}
+                    onChange={this.hangleChange}
+                    />
+                    <input
+                     type="text" 
+                     name="bottomText" 
+                     placeholder="Bottom text"
+                     value={this.state.bottomText}
+                    onChange={this.hangleChange}
+                    />
+                    <button>Gen</button>
+                </form>
             </div>
         )
     }
